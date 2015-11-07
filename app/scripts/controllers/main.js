76fd737fd3c2875203421649bd98f9771f9155df
Tobi Kehinde <tokehinde11@gmail.com>
2015-11-07 16:19:09 -0700

    Main controller unit tests added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('ngTodoApp')
   .controller('MainController', [ '$scope', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2','Item 3'];
+    $scope.todos = [];
 
     $scope.addTodo = function(){
       $scope.todos.push($scope.todo);
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -11,12 +11,25 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('MainController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should have no items to start', function () {
+    expect(scope.todos.length).toBe(0);
+  });
+
+  it('should add items to the list', function(){
+    scope.todo = 'Test 1';
+    scope.addTodo();
+    expect(scope.todos.length).toBe(1);
+  });
+
+  it('should add then remove an item from the list', function(){
+    scope.todo = "Test 1";
+    scope.addTodo();
+    scope.removeTodo(0);
+    expect(scope.todos.length).toBe(0);
   });
 });

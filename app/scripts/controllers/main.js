d3e916383b716d8441af5cce01e15fd1005d6e85
Tobi Kehinde <tokehinde11@gmail.com>
2015-11-07 17:59:54 -0700

    Local storage added to project. List items now persist to local storage.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,14 @@
  * Controller of the ngTodoApp
  */
 angular.module('ngTodoApp')
-  .controller('MainController', [ '$scope', function ($scope) {
-    $scope.todos = [];
+  .controller('MainController', [ '$scope','localStorageService', function ($scope,localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
 
     $scope.addTodo = function(){
       $scope.todos.push($scope.todo);

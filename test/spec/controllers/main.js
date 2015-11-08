325defc4e17bdd5eda4dc9bb81dd524fee76444b
Tobi Kehinde <tokehinde11@gmail.com>
2015-11-08 12:27:19 -0700

    Modifying controller unit test to use local storage

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -6,27 +6,48 @@ describe('Controller: MainCtrl', function () {
   beforeEach(module('ngTodoApp'));
 
   var MainCtrl,
-    scope;
+    scope,
+    localStorageService,
+    store;
+
+
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope,_localStorageService_) {
+    store = [];
     scope = $rootScope.$new();
+    localStorageService = _localStorageService_;
+
+    //mock localStorageService get/add
+    spyOn(localStorageService,'get').andCallFake(function(key){
+      return store[key];
+    });
+
+    spyOn(localStorageService,'add').andCallFake(function(key, val){
+      store[key] = val;
+    });
+
+    //Instantiate controller to test
     MainCtrl = $controller('MainController', {
-      $scope: scope
+      $scope: scope,
+      localStorageService: localStorageService
     });
   }));
 
-  it('should have no items to start', function () {
+  it('should retrieve todos from the store and assign to scope', function () {
+    expect(localStorageService.get).toHaveBeenCalledWith('todos');
     expect(scope.todos.length).toBe(0);
   });
 
   it('should add items to the list', function(){
     scope.todo = 'Test 1';
     scope.addTodo();
+    scope.$digest();
+    expect(localStorageService.add).toHaveBeenCalledWith('todos', jasmine.any(String));
     expect(scope.todos.length).toBe(1);
   });
 
-  it('should add then remove an item from the list', function(){
+  it('should add, then remove an item from the list and store', function(){
     scope.todo = 'Test 1';
     scope.addTodo();
     scope.removeTodo(0);

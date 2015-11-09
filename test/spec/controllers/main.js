ac8db25d7bac3fda377ff5ad2e6b9922a46d22d6
Tobi Kehinde <tokehinde11@gmail.com>
2015-11-09 06:28:48 -0700

    Local storage unit tests succeesfully implemented and passing

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,55 +1,62 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: MainController', function () {
 
   // load the controller's module
   beforeEach(module('ngTodoApp'));
 
-  var MainCtrl,
+  var MainController,
     scope,
     localStorageService,
     store;
 
-
-
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope,_localStorageService_) {
-    store = [];
+    store = {};
     scope = $rootScope.$new();
     localStorageService = _localStorageService_;
 
-    //mock localStorageService get/add
-    spyOn(localStorageService,'get').andCallFake(function(key){
+    //mock localStorageService get
+    spyOn(localStorageService,'get').and.callFake(function(key){
       return store[key];
     });
 
-    spyOn(localStorageService,'add').andCallFake(function(key, val){
+    //Object.defineProperty(sessionStorage, 'setItem', { writable: true });
+
+    //mock add item
+    spyOn(localStorageService,'set').and.callFake(function(key, val){
       store[key] = val;
     });
 
     //Instantiate controller to test
-    MainCtrl = $controller('MainController', {
+    MainController = $controller('MainController', {
       $scope: scope,
       localStorageService: localStorageService
     });
+
   }));
+
+  it("should have a $scope variable", function(){
+     expect(scope).toBeDefined();
+  });
 
   it('should retrieve todos from the store and assign to scope', function () {
     expect(localStorageService.get).toHaveBeenCalledWith('todos');
     expect(scope.todos.length).toBe(0);
   });
 
-  it('should add items to the list', function(){
+  it('should add an item to the list', function(){
     scope.todo = 'Test 1';
     scope.addTodo();
     scope.$digest();
-    expect(localStorageService.add).toHaveBeenCalledWith('todos', jasmine.any(String));
+    expect(localStorageService.set).toHaveBeenCalledWith('todos', scope.todos);
     expect(scope.todos.length).toBe(1);
   });
 
   it('should add, then remove an item from the list and store', function(){
     scope.todo = 'Test 1';
     scope.addTodo();
+    scope.$digest();
     scope.removeTodo(0);
     expect(scope.todos.length).toBe(0);
   });
